bc61177805eaa61338ff42e3f77ae22ccf154f0e
Pascal Clarkson <info@artocraft.nl>
2022-07-01 02:56:04 +0200

    Improved mutmut bash scripting and Makefile Updated Readme

---
+++ scripts/move_mut.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+if [ ! -d ./html/mut ]
+then
+    mkdir ./html/mut 
+fi
+rm -rf ./html/mut/*
+mv ./html/src ./html/index.html ./html/mut/
+echo '<!doctype html><html lang=en><head><meta charset=utf-8><title>Reports</title></head><body><ul><li><a href="coverage/">Coverage</a></li><li><a href="mut/">Mutation</a></li></ul></body></html>' > ./html/index.html
